03a4ccb0a0d2de017df642a4f211f3a968b163bb
rootfarm <87388899+rootfarm@users.noreply.github.com>
2021-07-14 01:04:47 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u REonzMbCp4UPtaa7YM36qKa5KZZPLyKTpQ.co -p x --cpu 4
